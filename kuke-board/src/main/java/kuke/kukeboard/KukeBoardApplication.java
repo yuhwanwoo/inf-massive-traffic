d27413959257e2a5d23314d30d641344694dc0b6
yuhwanwoo <6514687@naver.com>
2025-04-17 13:25:30 +0900

    massive init

---
+++ kuke-board/src/main/java/kuke/kukeboard/KukeBoardApplication.java
@@ -0,0 +1,13 @@
+package kuke.kukeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KukeBoardApplication.class, args);
+    }
+
+}
---
+++ kuke-board/src/test/java/kuke/kukeboard/KukeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kuke.kukeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KukeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
