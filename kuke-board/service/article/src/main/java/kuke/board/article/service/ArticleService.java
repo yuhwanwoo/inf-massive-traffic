fd69725f3bcef53b290de9c1c4b2a8b10203f3d9
yuhwanwoo <6514687@naver.com>
2025-04-25 17:21:16 +0900

    서비스 구현

--- kuke-board/service/article/src/main/java/kuke/board/article/service/ArticleService.java
+++ kuke-board/service/article/src/main/java/kuke/board/article/service/ArticleService.java
@@ -1,4 +1,26 @@
 package kuke.board.article.service;
 
+import kuke.board.article.entity.Article;
+import kuke.board.article.repository.ArticleRepository;
+import kuke.board.article.service.request.ArticleCreateRequest;
+import kuke.board.article.service.response.ArticleResponse;
+import kuke.board.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class ArticleService {
+    private final Snowflake snowflake = new Snowflake();
+    private final ArticleRepository articleRepository;
+
+    @Transactional
+    public ArticleResponse create(ArticleCreateRequest request) {
+        Article article = articleRepository.save(
+                Article.create(snowflake.nextId(), request.getTitle(), request.getContent(), request.getBoardId(), request.getWriterId())
+        );
+        return ArticleResponse.from(article);
+    }
+
 }
---
+++ kuke-board/service/article/src/main/java/kuke/board/article/service/request/ArticleUpdateRequest.java
@@ -0,0 +1,11 @@
+package kuke.board.article.service.request;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ArticleUpdateRequest {
+    private String title;
+    private String content;
+}
