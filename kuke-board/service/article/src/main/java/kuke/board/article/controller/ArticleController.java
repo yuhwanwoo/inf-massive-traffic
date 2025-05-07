3a4c529c4588db55dfa68a21a829837f1cec6df8
yuhwanwoo <6514687@naver.com>
2025-05-07 13:25:50 +0900

    api 개발

---
+++ kuke-board/service/article/src/main/java/kuke/board/article/controller/ArticleController.java
@@ -0,0 +1,40 @@
+package kuke.board.article.controller;
+
+import kuke.board.article.service.ArticleService;
+import kuke.board.article.service.request.ArticleCreateRequest;
+import kuke.board.article.service.request.ArticleUpdateRequest;
+import kuke.board.article.service.response.ArticleResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ArticleController {
+    private final ArticleService articleService;
+
+    @GetMapping("/v1/articles/{articleId}")
+    public ArticleResponse read(@PathVariable Long articleId) {
+        return articleService.read(articleId);
+    }
+
+    @PostMapping("/v1/articles")
+    public ArticleResponse create(@RequestBody ArticleCreateRequest request) {
+        return articleService.create(request);
+    }
+
+    @PutMapping("/v1/articles/{articleId}")
+    public ArticleResponse update(@PathVariable Long articleId, @RequestBody ArticleUpdateRequest request) {
+        return articleService.update(articleId, request);
+    }
+
+    @DeleteMapping("/v1/articles/{articleId}")
+    public void delete(@PathVariable Long articleId) {
+        articleService.delete(articleId);
+    }
+}
--- kuke-board/service/article/src/main/java/kuke/board/article/service/ArticleService.java
+++ kuke-board/service/article/src/main/java/kuke/board/article/service/ArticleService.java
@@ -3,6 +3,7 @@ package kuke.board.article.service;
 import kuke.board.article.entity.Article;
 import kuke.board.article.repository.ArticleRepository;
 import kuke.board.article.service.request.ArticleCreateRequest;
+import kuke.board.article.service.request.ArticleUpdateRequest;
 import kuke.board.article.service.response.ArticleResponse;
 import kuke.board.common.snowflake.Snowflake;
 import lombok.RequiredArgsConstructor;
@@ -23,4 +24,19 @@ public class ArticleService {
         return ArticleResponse.from(article);
     }
 
+    @Transactional
+    public ArticleResponse update(Long articleId, ArticleUpdateRequest request) {
+        Article article = articleRepository.findById(articleId).orElseThrow();
+        article.update(request.getTitle(), request.getContent());
+        return ArticleResponse.from(article);
+    }
+
+    public ArticleResponse read(Long articleId) {
+        return ArticleResponse.from(articleRepository.findById(articleId).orElseThrow());
+    }
+
+    @Transactional
+    public void delete(Long articleId) {
+        articleRepository.deleteById(articleId);
+    }
 }
