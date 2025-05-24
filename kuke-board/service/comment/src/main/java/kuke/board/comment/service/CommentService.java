a1ecb7c03119b6b5ab0ea393d6eda26b568d189a
yuhwanwoo <6514687@naver.com>
2025-05-24 22:25:45 +0900

    서비스 로직 구현

---
+++ kuke-board/service/comment/src/main/java/kuke/board/comment/service/CommentService.java
@@ -0,0 +1,78 @@
+package kuke.board.comment.service;
+
+import kuke.board.comment.entity.Comment;
+import kuke.board.comment.repository.CommentRepository;
+import kuke.board.comment.service.request.CommentCreateRequest;
+import kuke.board.comment.service.response.CommentResponse;
+import kuke.board.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Predicate;
+
+import static java.util.function.Predicate.not;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final Snowflake snowflake = new Snowflake();
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public CommentResponse create(CommentCreateRequest request) {
+        Comment parent = findParent(request);
+        Comment comment = commentRepository.save(
+                Comment.create(
+                        snowflake.nextId(),
+                        request.getContent(),
+                        parent == null ? null : parent.getCommentId(),
+                        request.getArticleId(),
+                        request.getWriterId()
+                )
+        );
+        return CommentResponse.from(comment);
+    }
+
+    private Comment findParent(CommentCreateRequest request) {
+        Long parentCommentId = request.getParentCommentId();
+        if (parentCommentId == null) {
+            return null;
+        }
+        return commentRepository.findById(parentCommentId)
+                .filter(not(Comment::getDeleted))
+                .filter(Comment::isRoot)
+                .orElseThrow();
+    }
+
+    public CommentResponse read(Long commentId) {
+        return CommentResponse.from(commentRepository.findById(commentId).orElseThrow());
+    }
+
+    @Transactional
+    public void delete(Long commentId) {
+        commentRepository.findById(commentId)
+                .filter(not(Comment::getDeleted))
+                .ifPresent(comment -> {
+                    if (hasChildren(comment)) {
+                        comment.delete();
+                    } else {
+                        delete(comment);
+                    }
+                });
+    }
+
+    private boolean hasChildren(Comment comment) {
+        return commentRepository.countBy(comment.getArticleId(), comment.getCommentId(), 2L) == 2;
+    }
+
+    private void delete(Comment comment) {
+        commentRepository.delete(comment);
+        if (!comment.isRoot()) {
+            commentRepository.findById(comment.getParentCommentId())
+                    .filter(Comment::getDeleted)
+                    .filter(not(this::hasChildren))
+                    .ifPresent(this::delete);
+        }
+    }
+}
