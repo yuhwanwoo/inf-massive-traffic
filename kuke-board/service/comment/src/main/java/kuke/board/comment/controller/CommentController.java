86b276f074005dc606eaba4058c95592bd1deeec
yuhwanwoo <6514687@naver.com>
2025-05-27 07:37:02 +0900

    controller 및 테스트 코드 작성

---
+++ kuke-board/service/comment/src/main/java/kuke/board/comment/controller/CommentController.java
@@ -0,0 +1,36 @@
+package kuke.board.comment.controller;
+
+import kuke.board.comment.service.CommentService;
+import kuke.board.comment.service.request.CommentCreateRequest;
+import kuke.board.comment.service.response.CommentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping("/v1/comments/{commentId}")
+    public CommentResponse read(
+            @PathVariable("commentId") Long commentId
+    ) {
+        return commentService.read(commentId);
+    }
+
+    @PostMapping("/v1/comments")
+    public CommentResponse create(@RequestBody CommentCreateRequest request) {
+        return commentService.create(request);
+    }
+
+    @DeleteMapping("/v1/comments/{commentId}")
+    public void delete(@PathVariable("commentId") Long commentId) {
+        commentService.delete(commentId);
+    }
+
+}
