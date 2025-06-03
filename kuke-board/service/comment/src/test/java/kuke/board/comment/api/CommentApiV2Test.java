ff308b3157d602f7e0a7209dc88d29ced01d31c6
yuhwanwoo <6514687@naver.com>
2025-06-03 18:00:28 +0900

    데이터 추가 로직 추가

---
+++ kuke-board/service/comment/src/test/java/kuke/board/comment/api/CommentApiV2Test.java
@@ -0,0 +1,69 @@
+package kuke.board.comment.api;
+
+import kuke.board.comment.service.request.CommentCreateRequestV2;
+import kuke.board.comment.service.response.CommentResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class CommentApiV2Test {
+    RestClient restClient = RestClient.create("http://localhost:9001");
+
+    @Test
+    void create() {
+        CommentResponse response1 = create(new CommentCreateRequestV2(1L, "my comment1", null, 1L));
+        CommentResponse response2 = create(new CommentCreateRequestV2(1L, "my comment2", response1.getPath(), 1L));
+        CommentResponse response3 = create(new CommentCreateRequestV2(1L, "my comment3", response2.getPath(), 1L));
+
+        System.out.println("response1.getPath() = " + response1.getPath());
+        System.out.println("response1.getCommentId() = " + response1.getCommentId());
+        System.out.println("\tresponse2.getPath() = " + response2.getPath());
+        System.out.println("\tresponse2.getCommentId() = " + response2.getCommentId());
+        System.out.println("\t\tresponse3.getPath() = " + response3.getPath());
+        System.out.println("\t\tresponse3.getCommentId() = " + response3.getCommentId());
+
+        /**
+         * response1.getPath() = 00002
+         * response1.getCommentId() = 187809049973350400
+         * 	response2.getPath() = 0000200000
+         * 	response2.getCommentId() = 187809050212425728
+         * 		response3.getPath() = 000020000000000
+         * 		response3.getCommentId() = 187809050287923200
+         */
+
+    }
+
+    CommentResponse create(CommentCreateRequestV2 request) {
+        return restClient.post()
+                .uri("/v2/comments")
+                .body(request)
+                .retrieve()
+                .body(CommentResponse.class);
+    }
+
+    @Test
+    void read() {
+        CommentResponse response = restClient.get()
+                .uri("/v2/comments/{commentId}", 187809049973350400L)
+                .retrieve()
+                .body(CommentResponse.class);
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void delete() {
+        restClient.delete()
+                .uri("/v2/comments/{commentId}", 187809049973350400L)
+                .retrieve();
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class CommentCreateRequestV2 {
+        private Long articleId;
+        private String content;
+        private String parentPath;
+        private Long writerId;
+    }
+}
