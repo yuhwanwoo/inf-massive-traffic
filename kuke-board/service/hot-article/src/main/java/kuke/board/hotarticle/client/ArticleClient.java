f30acf9fa2c64c47b968a35dc30d646ff305ad2b
yuhwanwoo <6514687@naver.com>
2025-06-23 08:27:25 +0900

    카프카 설정 추가

---
+++ kuke-board/service/hot-article/src/main/java/kuke/board/hotarticle/client/ArticleClient.java
@@ -0,0 +1,9 @@
+package kuke.board.hotarticle.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestClient;
+
+public class ArticleClient {
+    private RestClient restClient;
+//    @Value("")
+}
