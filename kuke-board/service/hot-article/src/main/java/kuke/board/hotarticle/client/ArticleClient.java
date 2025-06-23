7eb56b289b7a02d045bdb9d91167bc35c23c597f
yuhwanwoo <6514687@naver.com>
2025-06-23 09:04:58 +0900

    sorted set을 이용한 랭킹 처리

--- kuke-board/service/hot-article/src/main/java/kuke/board/hotarticle/client/ArticleClient.java
+++ kuke-board/service/hot-article/src/main/java/kuke/board/hotarticle/client/ArticleClient.java
@@ -1,9 +1,41 @@
 package kuke.board.hotarticle.client;
 
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestClient;
 
+import java.time.LocalDateTime;
+
 public class ArticleClient {
+    private static final Logger log = LoggerFactory.getLogger(ArticleClient.class);
     private RestClient restClient;
-//    @Value("")
+    @Value("${endpoints.kuke-board-article-service.url}")
+    private String articleServiceUrl;
+
+    @PostConstruct
+    void initRestClient() {
+        restClient = RestClient.create(articleServiceUrl);
+    }
+
+    public ArticleResponse read(Long articleId) {
+        try {
+            return restClient.get()
+                    .uri("/v1/articles/{articleId}", articleId)
+                    .retrieve()
+                    .body(ArticleResponse.class);
+        } catch (Exception e) {
+            log.error("[ArticleClient.read] articleId: {}", articleId, e);
+        }
+        return null;
+    }
+
+    @Getter
+    public static class ArticleResponse {
+        private Long articleId;
+        private String title;
+        private LocalDateTime createdAt;
+    }
 }
