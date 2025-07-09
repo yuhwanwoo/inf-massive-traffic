5fabd180f4822df505a6de9e28a7f1b3d3c00ac4
yuhwanwoo <6514687@naver.com>
2025-07-09 12:39:59 +0900

    카프카 로직 구현

---
+++ kuke-board/common/outbox-message-relay/src/main/java/kuke/board/common/outboxmessagerelay/OutboxRepository.java
@@ -0,0 +1,15 @@
+package kuke.board.common.outboxmessagerelay;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface OutboxRepository extends JpaRepository<Outbox, Long> {
+    List<Outbox> findAllByShardKeyAndCreatedAtLessThanEqualOrderByCreatedAtAsc(
+            Long shardKey,
+            LocalDateTime from,
+            Pageable pageable
+    );
+}
