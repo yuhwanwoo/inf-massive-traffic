bbeed80993ec7ae09ff4adae5160c643c21adb78
yuhwanwoo <6514687@naver.com>
2025-07-25 20:45:06 +0900

    핸들러 로직 추가

--- kuke-board/service/article-read/src/main/java/kuke/board/articleread/service/event/handler/ArticleCreatedEventHandler.java
+++ kuke-board/service/article-read/src/main/java/kuke/board/articleread/service/event/handler/ArticleCreatedEventHandler.java
@@ -1,7 +1,9 @@
 package kuke.board.articleread.service.event.handler;
 
+import kuke.board.articleread.repository.ArticleIdListRepository;
 import kuke.board.articleread.repository.ArticleQueryModel;
 import kuke.board.articleread.repository.ArticleQueryModelRepository;
+import kuke.board.articleread.repository.BoardArticleCountRepository;
 import kuke.board.common.event.Event;
 import kuke.board.common.event.EventType;
 import kuke.board.common.event.payload.ArticleCreatedEventPayload;
@@ -13,7 +15,9 @@ import java.time.Duration;
 @Component
 @RequiredArgsConstructor
 public class ArticleCreatedEventHandler implements EventHandler<ArticleCreatedEventPayload> {
+    private final ArticleIdListRepository articleIdListRepository;
     private final ArticleQueryModelRepository articleQueryModelRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
     @Override
     public void handle(Event<ArticleCreatedEventPayload> event) {
         ArticleCreatedEventPayload payload = event.getPayload();
@@ -21,6 +25,8 @@ public class ArticleCreatedEventHandler implements EventHandler<ArticleCreatedEv
                         ArticleQueryModel.create(payload),
                         Duration.ofDays(1)
                 );
+        articleIdListRepository.add(payload.getBoardId(), payload.getArticleId(), 1000L);
+        boardArticleCountRepository.createOrUpdate(payload.getBoardId(), payload.getBoardArticleCount());
     }
 
     @Override
--- kuke-board/service/article-read/src/main/java/kuke/board/articleread/service/event/handler/ArticleDeletedEventHandler.java
+++ kuke-board/service/article-read/src/main/java/kuke/board/articleread/service/event/handler/ArticleDeletedEventHandler.java
@@ -1,6 +1,8 @@
 package kuke.board.articleread.service.event.handler;
 
+import kuke.board.articleread.repository.ArticleIdListRepository;
 import kuke.board.articleread.repository.ArticleQueryModelRepository;
+import kuke.board.articleread.repository.BoardArticleCountRepository;
 import kuke.board.common.event.Event;
 import kuke.board.common.event.EventType;
 import kuke.board.common.event.payload.ArticleDeletedEventPayload;
@@ -11,11 +13,15 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ArticleDeletedEventHandler implements EventHandler<ArticleDeletedEventPayload> {
+    private final ArticleIdListRepository articleIdListRepository;
     private final ArticleQueryModelRepository articleQueryModelRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
     @Override
     public void handle(Event<ArticleDeletedEventPayload> event) {
         ArticleDeletedEventPayload payload = event.getPayload();
         articleQueryModelRepository.delete(payload.getArticleId());
+        articleIdListRepository.delete(payload.getBoardId(), payload.getArticleId());
+        boardArticleCountRepository.createOrUpdate(payload.getBoardId(), payload.getBoardArticleCount());
     }
 
     @Override
