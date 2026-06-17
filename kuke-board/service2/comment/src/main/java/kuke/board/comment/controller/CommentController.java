0a4e0b9318e8e359393a59378be87ef287623d62
yuhwanwoo <6514687@naver.com>
2026-06-18 08:58:54 +0900

    comment controller add

---
+++ kuke-board/service2/comment/src/main/java/kuke/board/comment/controller/CommentController.java
@@ -0,0 +1,53 @@
+package kuke.board.comment.controller;
+
+import kuke.board.comment.service.CommentService;
+import kuke.board.comment.service.request.CommentCreateRequest;
+import kuke.board.comment.service.response.CommentPageResponse;
+import kuke.board.comment.service.response.CommentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping("/v1/comments/{commentId}")
+    public CommentResponse read(
+            @PathVariable("commentId") Long commentId
+    ) {
+        return commentService.read(commentId);
+    }
+
+    @PostMapping("/v1/comments")
+    public CommentResponse create(@RequestBody CommentCreateRequest request) {
+        return commentService.create(request);
+    }
+
+    @DeleteMapping("/v1/comments/{commentId}")
+    public void delete(@PathVariable("commentId") Long commentId) {
+        commentService.delete(commentId);
+    }
+
+    @GetMapping("/v1/comments")
+    public CommentPageResponse read(
+            @RequestParam("articleId") Long articleId,
+            @RequestParam("page") Long page,
+            @RequestParam("pageSize") Long pageSize
+    ) {
+        return commentService.readAll(articleId, page, pageSize);
+    }
+
+    @GetMapping("/v1/comments/infinite-scroll")
+    public List<CommentResponse> read(
+            @RequestParam("articleId") Long articleId,
+            @RequestParam(value = "lastParentCommentId", required = false) Long lastParentCommentId,
+            @RequestParam(value = "lastCommentId", required = false) Long lastCommentId,
+            @RequestParam("pageSize") Long pageSize
+    ) {
+        return commentService.readAll(articleId, lastParentCommentId, lastCommentId, pageSize);
+    }
+
+}
