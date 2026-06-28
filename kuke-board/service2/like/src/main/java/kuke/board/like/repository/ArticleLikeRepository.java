9108b9e7365171a11b48bd1557dfc673e1fd3fc4
yuhwanwoo <6514687@naver.com>
2026-06-28 09:21:23 +0900

    article like init

---
+++ kuke-board/service2/like/src/main/java/kuke/board/like/repository/ArticleLikeRepository.java
@@ -0,0 +1,10 @@
+package kuke.board.like.repository;
+
+import kuke.board.like.entity.ArticleLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
+    Optional<ArticleLike> findByArticleIdAndUserId(Long articleId, Long userId);
+}
