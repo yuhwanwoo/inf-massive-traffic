2209d826f073bd4e05078f2576d402548175817c
yuhwanwoo <6514687@naver.com>
2026-07-20 09:27:54 +0900

    인기글 점수 계산기 추가

---
+++ kuke-board/service2/hot-article/src/main/java/kuke/board/hotarticle/service/HotArticleScoreCalculator.java
@@ -0,0 +1,29 @@
+package kuke.board.hotarticle.service;
+
+import kuke.board.hotarticle.repository.ArticleCommentCountRepository;
+import kuke.board.hotarticle.repository.ArticleLikeCountRepository;
+import kuke.board.hotarticle.repository.ArticleViewCountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class HotArticleScoreCalculator {
+    private final ArticleLikeCountRepository articleLikeCountRepository;
+    private final ArticleViewCountRepository articleViewCountRepository;
+    private final ArticleCommentCountRepository articleCommentCountRepository;
+
+    private static final long ARTICLE_LIKE_COUNT_WEIGHT = 3;
+    private static final long ARTICLE_COMMENT_COUNT_WEIGHT = 2;
+    private static final long ARTICLE_VIEW_COUNT_WEIGHT = 1;
+
+    public long calculate(Long articleId) {
+        Long articleLikeCount = articleLikeCountRepository.read(articleId);
+        Long articleViewCount = articleViewCountRepository.read(articleId);
+        Long articleCommentCount = articleCommentCountRepository.read(articleId);
+
+        return articleLikeCount * ARTICLE_LIKE_COUNT_WEIGHT
+                + articleViewCount * ARTICLE_VIEW_COUNT_WEIGHT
+                + articleCommentCount * ARTICLE_COMMENT_COUNT_WEIGHT;
+    }
+}
