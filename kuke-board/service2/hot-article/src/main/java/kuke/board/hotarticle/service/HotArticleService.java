56d1be4c7408fef736c43664db03fb814e49f079
yuhwanwoo <6514687@naver.com>
2026-07-24 15:56:05 +0900

    인기글 서비스 추가

---
+++ kuke-board/service2/hot-article/src/main/java/kuke/board/hotarticle/service/HotArticleService.java
@@ -0,0 +1,58 @@
+package kuke.board.hotarticle.service;
+
+import kuke.board.common.event.Event;
+import kuke.board.common.event.EventPayload;
+import kuke.board.common.event.EventType;
+import kuke.board.hotarticle.client.ArticleClient;
+import kuke.board.hotarticle.repository.HotArticleListRepository;
+import kuke.board.hotarticle.service.eventhandler.EventHandler;
+
+import kuke.board.hotarticle.service.response.HotArticleResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class HotArticleService {
+    private final ArticleClient articleClient;
+    private final List<EventHandler> eventHandlers;
+    private final HotArticleScoreUpdater hotArticleScoreUpdater;
+    private final HotArticleListRepository hotArticleListRepository;
+
+    public void handlerEvent(Event<EventPayload> event) {
+        EventHandler<EventPayload> eventHandler = findEventHandler(event);
+        if (eventHandler == null) {
+            return;
+        }
+
+        if (isArticleCreatedOrDeleted(event)) {
+            eventHandler.handle(event);
+        } else {
+            hotArticleScoreUpdater.update(event, eventHandler);
+        }
+    }
+
+    private EventHandler<EventPayload> findEventHandler(Event<EventPayload> event) {
+        return eventHandlers.stream()
+                .filter(eventHandler -> eventHandler.supports(event))
+                .findAny()
+                .orElse(null);
+    }
+
+    private boolean isArticleCreatedOrDeleted(Event<EventPayload> event) {
+        return EventType.ARTICLE_CREATED == event.getType() || EventType.ARTICLE_DELETED == event.getType();
+    }
+
+    public List<HotArticleResponse> readAll(String dateStr) {
+        return hotArticleListRepository.readAll(dateStr).stream()
+                .map(articleClient::read)
+                .filter(Objects::nonNull)
+                .map(HotArticleResponse::from)
+                .toList();
+    }
+}
