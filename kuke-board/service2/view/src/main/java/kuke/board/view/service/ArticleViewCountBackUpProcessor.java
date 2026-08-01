71636e8daa1563507e86515402b94e5b6d20a43a
yuhwanwoo <6514687@naver.com>
2026-08-01 16:18:47 +0900

    조회수 백업 프로세서 추가

---
+++ kuke-board/service2/view/src/main/java/kuke/board/view/service/ArticleViewCountBackUpProcessor.java
@@ -0,0 +1,36 @@
+package kuke.board.view.service;
+
+import kuke.board.common.event.EventType;
+import kuke.board.common.event.payload.ArticleViewedEventPayload;
+import kuke.board.common.outboxmessagerelay.OutboxEventPublisher;
+import kuke.board.view.entity.ArticleViewCount;
+import kuke.board.view.repository.ArticleViewCountBackUpRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleViewCountBackUpProcessor {
+    private final OutboxEventPublisher outboxEventPublisher;
+    private final ArticleViewCountBackUpRepository articleViewCountBackUpRepository;
+
+    @Transactional
+    public void backUp(Long articleId, Long viewCount) {
+        int result = articleViewCountBackUpRepository.updateViewCount(articleId, viewCount);
+        if (result == 0) {
+            articleViewCountBackUpRepository.findById(articleId)
+                    .ifPresentOrElse(ignored -> {},
+                            () -> articleViewCountBackUpRepository.save(ArticleViewCount.init(articleId, viewCount))
+                    );
+        }
+        outboxEventPublisher.publish(
+                EventType.ARTICLE_VIEWED,
+                ArticleViewedEventPayload.builder()
+                        .articleId(articleId)
+                        .articleViewCount(viewCount)
+                        .build(),
+                articleId
+        );
+    }
+}
