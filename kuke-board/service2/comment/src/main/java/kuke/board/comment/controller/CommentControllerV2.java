add4991856b89bcab390156ea23a3a9e93908c45
yuhwanwoo <6514687@naver.com>
2026-08-05 10:05:23 +0900

    댓글 컨트롤러 추가

---
+++ kuke-board/service2/comment/src/main/java/kuke/board/comment/controller/CommentControllerV2.java
@@ -0,0 +1,58 @@
+package kuke.board.comment.controller;
+
+import kuke.board.comment.service.CommentServiceV2;
+import kuke.board.comment.service.request.CommentCreateRequestV2;
+import kuke.board.comment.service.response.CommentPageResponse;
+import kuke.board.comment.service.response.CommentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentControllerV2 {
+    private final CommentServiceV2 commentService;
+
+    @GetMapping("/v2/comments/{commentId}")
+    public CommentResponse read(
+            @PathVariable("commentId") Long commentId
+    ) {
+        return commentService.read(commentId);
+    }
+
+    @PostMapping("/v2/comments")
+    public CommentResponse create(@RequestBody CommentCreateRequestV2 request) {
+        return commentService.create(request);
+    }
+
+    @DeleteMapping("/v2/comments/{commentId}")
+    public void delete(@PathVariable("commentId") Long commentId) {
+        commentService.delete(commentId);
+    }
+
+    @GetMapping("/v2/comments")
+    public CommentPageResponse readAll(
+            @RequestParam("articleId") Long articleId,
+            @RequestParam("page") Long page,
+            @RequestParam("pageSize") Long pageSize
+    ) {
+        return commentService.readAll(articleId, page, pageSize);
+    }
+
+    @GetMapping("/v2/comments/infinite-scroll")
+    public List<CommentResponse> readAll(
+            @RequestParam("articleId") Long articleId,
+            @RequestParam(value = "lastPath", required = false) String lastPath,
+            @RequestParam("pageSize") Long pageSize
+    ) {
+        return commentService.readAllInfiniteScroll(articleId, lastPath, pageSize);
+    }
+
+    @GetMapping("/v2/comments/articles/{articleId}/count")
+    public Long count(
+            @PathVariable("articleId") Long articleId
+    ) {
+        return commentService.count(articleId);
+    }
+}
