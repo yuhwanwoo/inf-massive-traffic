699b6953999c410e9f188b3e0e6f5e76d6a1d3ac
yuhwanwoo <6514687@naver.com>
2026-08-13 21:04:29 -0400

    게시글 컨트롤러 추가

---
+++ kuke-board/service2/article/src/main/java/kuke/board/article/controller/ArticleController.java
@@ -0,0 +1,59 @@
+package kuke.board.article.controller;
+
+import kuke.board.article.service.ArticleService;
+import kuke.board.article.service.request.ArticleCreateRequest;
+import kuke.board.article.service.request.ArticleUpdateRequest;
+import kuke.board.article.service.response.ArticlePageResponse;
+import kuke.board.article.service.response.ArticleResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ArticleController {
+    private final ArticleService articleService;
+
+    @GetMapping("/v1/articles/{articleId}")
+    public ArticleResponse read(@PathVariable Long articleId) {
+        return articleService.read(articleId);
+    }
+
+    @GetMapping("/v1/articles")
+    public ArticlePageResponse readAll(
+            @RequestParam("boardId") Long boardId,
+            @RequestParam("page") Long page,
+            @RequestParam("pageSize") Long pageSize
+    ) {
+        return articleService.readAll(boardId, page, pageSize);
+    }
+
+    @GetMapping("/v1/articles/infinite-scroll")
+    public List<ArticleResponse> readAllInfiniteScroll(
+            @RequestParam("boardId") Long boardId,
+            @RequestParam("pageSize") Long pageSize,
+            @RequestParam(value = "lastArticleId", required = false) Long lastArticleId) {
+        return articleService.readAllInfiniteScroll(boardId, pageSize, lastArticleId);
+    }
+
+    @PostMapping("/v1/articles")
+    public ArticleResponse create(@RequestBody ArticleCreateRequest request) {
+        return articleService.create(request);
+    }
+
+    @PutMapping("/v1/articles/{articleId}")
+    public ArticleResponse update(@PathVariable Long articleId, @RequestBody ArticleUpdateRequest request) {
+        return articleService.update(articleId, request);
+    }
+
+    @DeleteMapping("/v1/articles/{articleId}")
+    public void delete(@PathVariable Long articleId) {
+        articleService.delete(articleId);
+    }
+
+    @GetMapping("/v1/articles/boards/{boardId}/count")
+    public Long count(@PathVariable Long boardId) {
+        return articleService.count(boardId);
+    }
+}
