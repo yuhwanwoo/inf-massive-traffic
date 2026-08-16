e24376e1a09ae597a0384d8502e488657fe6268f
yuhwanwoo <6514687@naver.com>
2026-08-15 18:35:28 -0700

    조회수 클라이언트 추가

---
+++ kuke-board/service2/article-read/src/main/java/kuke/board/articleread/client/ViewClient.java
@@ -0,0 +1,40 @@
+package kuke.board.articleread.client;
+
+import jakarta.annotation.PostConstruct;
+import kuke.board.articleread.cache.OptimizedCacheable;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ViewClient {
+    private RestClient restClient;
+    @Value("${endpoints.kuke-board-view-service.url}")
+    private String viewServiceUrl;
+
+    @PostConstruct
+    public void initRestClient() {
+        restClient = RestClient.create(viewServiceUrl);
+    }
+
+//    @Cacheable(key = "#articleId", value = "articleViewCount")
+    @OptimizedCacheable(type = "articleViewCount", ttlSeconds = 1)
+    public Long count(Long articleId) {
+        log.info("[ViewClient.count] articleId={}", articleId);
+        try {
+            return restClient.get()
+                    .uri("/v1/article-views/articles/{articleId}/count", articleId)
+                    .retrieve()
+                    .body(Long.class);
+        } catch (Exception e) {
+            log.error("[ViewClient.count] articleId={}", articleId, e);
+            return 0L;
+        }
+    }
+
+
+}
