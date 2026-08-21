538d86d28e71497f00e64cf06a7884717a4f8749
yuhwanwoo <6514687@naver.com>
2026-08-21 18:01:17 +0900

    모델 추가

---
+++ kuke-board/service2/article-read/src/main/java/kuke/board/articleread/repository/ArticleQueryModel.java
@@ -0,0 +1,73 @@
+package kuke.board.articleread.repository;
+
+import kuke.board.articleread.client.ArticleClient;
+import kuke.board.common.event.payload.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ArticleQueryModel {
+    private Long articleId;
+    private String title;
+    private String content;
+    private Long boardId;
+    private Long writerId;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private Long articleCommentCount;
+    private Long articleLikeCount;
+
+    public static ArticleQueryModel create(ArticleCreatedEventPayload payload) {
+        ArticleQueryModel articleQueryModel = new ArticleQueryModel();
+        articleQueryModel.articleId = payload.getArticleId();
+        articleQueryModel.title = payload.getTitle();
+        articleQueryModel.content = payload.getContent();
+        articleQueryModel.boardId = payload.getBoardId();
+        articleQueryModel.writerId = payload.getWriterId();
+        articleQueryModel.createdAt = payload.getCreatedAt();
+        articleQueryModel.modifiedAt = payload.getModifiedAt();
+        articleQueryModel.articleCommentCount = 0L;
+        articleQueryModel.articleLikeCount = 0L;
+        return articleQueryModel;
+    }
+
+    public static ArticleQueryModel create(ArticleClient.ArticleResponse article, Long commentCount, Long likeCount) {
+        ArticleQueryModel articleQueryModel = new ArticleQueryModel();
+        articleQueryModel.articleId = article.getArticleId();
+        articleQueryModel.title = article.getTitle();
+        articleQueryModel.content = article.getContent();
+        articleQueryModel.boardId = article.getBoardId();
+        articleQueryModel.writerId = article.getWriterId();
+        articleQueryModel.createdAt = article.getCreatedAt();
+        articleQueryModel.modifiedAt = article.getModifiedAt();
+        articleQueryModel.articleCommentCount = commentCount;
+        articleQueryModel.articleLikeCount = likeCount;
+        return articleQueryModel;
+    }
+
+    public void updateBy(CommentCreatedEventPayload payload) {
+        this.articleCommentCount = payload.getArticleCommentCount();
+    }
+
+    public void updateBy(CommentDeletedEventPayload payload) {
+        this.articleCommentCount = payload.getArticleCommentCount();
+    }
+
+    public void updateBy(ArticleLikedEventPayload payload) {
+        this.articleLikeCount = payload.getArticleLikeCount();
+    }
+
+    public void updateBy(ArticleUnlikedEventPayload payload) {
+        this.articleLikeCount = payload.getArticleLikeCount();
+    }
+
+    public void updateBy(ArticleUpdatedEventPayload payload) {
+        this.title = payload.getTitle();
+        this.content = payload.getContent();
+        this.boardId = payload.getBoardId();
+        this.writerId = payload.getWriterId();
+        this.createdAt = payload.getCreatedAt();
+        this.modifiedAt = payload.getModifiedAt();
+    }
+}
