3eba66424116919b19b37976b10c21719c7fdd2b
yuhwanwoo <6514687@naver.com>
2026-08-26 13:28:19 +0900

    게시글 생성 이벤트 핸들러 생성

---
+++ kuke-board/service2/article-read/src/main/java/kuke/board/articleread/service/event/handler/ArticleCreatedEventHandler.java
@@ -0,0 +1,36 @@
+package kuke.board.articleread.service.event.handler;
+
+import kuke.board.articleread.repository.ArticleIdListRepository;
+import kuke.board.articleread.repository.ArticleQueryModel;
+import kuke.board.articleread.repository.ArticleQueryModelRepository;
+import kuke.board.articleread.repository.BoardArticleCountRepository;
+import kuke.board.common.event.Event;
+import kuke.board.common.event.EventType;
+import kuke.board.common.event.payload.ArticleCreatedEventPayload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleCreatedEventHandler implements EventHandler<ArticleCreatedEventPayload> {
+    private final ArticleIdListRepository articleIdListRepository;
+    private final ArticleQueryModelRepository articleQueryModelRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
+    @Override
+    public void handle(Event<ArticleCreatedEventPayload> event) {
+        ArticleCreatedEventPayload payload = event.getPayload();
+        articleQueryModelRepository.create(
+                        ArticleQueryModel.create(payload),
+                        Duration.ofDays(1)
+                );
+        articleIdListRepository.add(payload.getBoardId(), payload.getArticleId(), 1000L);
+        boardArticleCountRepository.createOrUpdate(payload.getBoardId(), payload.getBoardArticleCount());
+    }
+
+    @Override
+    public boolean supports(Event<ArticleCreatedEventPayload> event) {
+        return EventType.ARTICLE_CREATED == event.getType();
+    }
+}
