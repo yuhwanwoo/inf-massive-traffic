cf67392975a4e67ac427865845f16bc7f196e1c1
yuhwanwoo <6514687@naver.com>
2026-08-29 12:52:59 +0900

    댓글 생성 이벤트 핸들러 추가

---
+++ kuke-board/service2/article-read/src/main/java/kuke/board/articleread/service/event/handler/CommentCreatedEventHandler.java
@@ -0,0 +1,27 @@
+package kuke.board.articleread.service.event.handler;
+
+import kuke.board.articleread.repository.ArticleQueryModelRepository;
+import kuke.board.common.event.Event;
+import kuke.board.common.event.EventType;
+import kuke.board.common.event.payload.CommentCreatedEventPayload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommentCreatedEventHandler implements EventHandler<CommentCreatedEventPayload> {
+    private final ArticleQueryModelRepository articleQueryModelRepository;
+    @Override
+    public void handle(Event<CommentCreatedEventPayload> event) {
+        articleQueryModelRepository.read(event.getPayload().getArticleId())
+                .ifPresent(articleQueryModel -> {
+                    articleQueryModel.updateBy(event.getPayload());
+                    articleQueryModelRepository.update(articleQueryModel);
+                });
+    }
+
+    @Override
+    public boolean supports(Event<CommentCreatedEventPayload> event) {
+        return EventType.COMMENT_CREATED == event.getType();
+    }
+}
